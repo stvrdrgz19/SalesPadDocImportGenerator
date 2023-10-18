7222387bd11c232d30ce9c32308d92f315295ad4
stvrdrgz19 <stvrdrgz@gmail.com>
2023-10-18 15:30:17 -0400

    updates

--- classes/customers.py
+++ classes/customers.py
@@ -9,47 +9,47 @@ class Customer:
 
     def get_customer_list():
         networks0001 = Customer("NETWORKS0001", "Increase", "Invoice")
-        atmorere0001 = Customer("ATMORERE0001", "Decrease", "OrderInvoice")
-        unifiedw0001 = Customer("UNIFIEDW0001", "UpDown", "OrderInvoicePartial")
-        adampark0001 = Customer("ADAMPARK0001", "DownUp", "OrderInvoiceSplit")
-        midcityh0001 = Customer("MIDCITYH0001", "Wave", "Invoice")
-        westside0001 = Customer("WESTSIDE0001", "Seasonal", "OrderInvoice")
-        greenway0001 = Customer("GREENWAY0001", "Churn", "OrderInvoicePartial")
-        margiest0001 = Customer("MARGIEST0001", "Increase", "OrderInvoiceSplit")
-        northern0001 = Customer("NORTHERN0001", "Decrease", "Invoice")
-        blueyond0001 = Customer("BLUEYOND0001", "UpDown", "OrderInvoice")
-        directma0001 = Customer("DIRECTMA0001", "DownUp", "OrderInvoicePartial")
-        lasermes0001 = Customer("LASERMES0001", "Wave", "OrderInvoiceSplit")
-        northcol0001 = Customer("NORTHCOL0001", "Seasonal", "Invoice")
-        homefurn0001 = Customer("HOMEFURN0001", "Churn", "OrderInvoice")
-        astorsui0001 = Customer("ASTORSUI0001", "Increase", "OrderInvoicePartial")
-        cellular0001 = Customer("CELLULAR0001", "Decrease", "OrderInvoiceSplit")
-        aaronfit0001 = Customer("AARONFIT0001", "UpDown", "Invoice")
-        cohowine0001 = Customer("COHOWINE0001", "DownUp", "OrderInvoice")
-        contosol0001 = Customer("CONTOSOL0001", "Wave", "OrderInvoicePartial")
-        franchis0001 = Customer("FRANCHIS0001", "Seasonal", "OrderInvoiceSplit")
+        # atmorere0001 = Customer("ATMORERE0001", "Decrease", "OrderInvoice")
+        # unifiedw0001 = Customer("UNIFIEDW0001", "UpDown", "OrderInvoicePartial")
+        # adampark0001 = Customer("ADAMPARK0001", "DownUp", "OrderInvoiceSplit")
+        # midcityh0001 = Customer("MIDCITYH0001", "Wave", "Invoice")
+        # westside0001 = Customer("WESTSIDE0001", "Seasonal", "OrderInvoice")
+        # greenway0001 = Customer("GREENWAY0001", "Churn", "OrderInvoicePartial")
+        # margiest0001 = Customer("MARGIEST0001", "Increase", "OrderInvoiceSplit")
+        # northern0001 = Customer("NORTHERN0001", "Decrease", "Invoice")
+        # blueyond0001 = Customer("BLUEYOND0001", "UpDown", "OrderInvoice")
+        # directma0001 = Customer("DIRECTMA0001", "DownUp", "OrderInvoicePartial")
+        # lasermes0001 = Customer("LASERMES0001", "Wave", "OrderInvoiceSplit")
+        # northcol0001 = Customer("NORTHCOL0001", "Seasonal", "Invoice")
+        # homefurn0001 = Customer("HOMEFURN0001", "Churn", "OrderInvoice")
+        # astorsui0001 = Customer("ASTORSUI0001", "Increase", "OrderInvoicePartial")
+        # cellular0001 = Customer("CELLULAR0001", "Decrease", "OrderInvoiceSplit")
+        # aaronfit0001 = Customer("AARONFIT0001", "UpDown", "Invoice")
+        # cohowine0001 = Customer("COHOWINE0001", "DownUp", "OrderInvoice")
+        # contosol0001 = Customer("CONTOSOL0001", "Wave", "OrderInvoicePartial")
+        # franchis0001 = Customer("FRANCHIS0001", "Seasonal", "OrderInvoiceSplit")
 
         customer_list = [
-            networks0001,
-            atmorere0001,
-            unifiedw0001,
-            adampark0001,
-            midcityh0001,
-            westside0001,
-            greenway0001,
-            margiest0001,
-            northern0001,
-            blueyond0001,
-            directma0001,
-            lasermes0001,
-            northcol0001,
-            homefurn0001,
-            astorsui0001,
-            cellular0001,
-            aaronfit0001,
-            cohowine0001,
-            contosol0001,
-            franchis0001
+            networks0001
+            # atmorere0001,
+            # unifiedw0001,
+            # adampark0001,
+            # midcityh0001,
+            # westside0001,
+            # greenway0001,
+            # margiest0001,
+            # northern0001,
+            # blueyond0001,
+            # directma0001,
+            # lasermes0001,
+            # northcol0001,
+            # homefurn0001,
+            # astorsui0001,
+            # cellular0001,
+            # aaronfit0001,
+            # cohowine0001,
+            # contosol0001,
+            # franchis0001
         ]
 
         return customer_list
--- test.py
+++ test.py
@@ -1,52 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from collections import Counter
-import GenerateDocumentImportFiles
+import json
 
-def trend(count, start_weight, end_weight):
-    lin_sp = np.linspace(start_weight, end_weight, count)
-    return lin_sp/sum(lin_sp)
+with open("configuration/settings.json", "r") as file:
+    data = json.load(file)
 
-# gets a number of dates between a range based off of a trend
-def get_dates(start, end, doc_count, start_weight, end_weight):
-    dates = pd.date_range(start, end, freq="B")
-    date_trends = np.random.choice(dates, size=doc_count, p=trend(len(dates), start_weight, end_weight))
-    return date_trends
+next_doc_num = data["next_doc_num"]
+print(f'Next Doc Num: {next_doc_num}')
 
-dates = get_dates("2021-10-10", "2023-10-20", 1000, 1, 15)
+next_doc_num += 1
+data["next_doc_num"] = next_doc_num
 
-# sorted_indices = np.argsort(dates)
+with open("configuration/settings.json", "w") as file:
+    json.dump(data, file, indent=4)
 
-# sorted_dates = [dates[i] for i in sorted_indices]
+with open("configuration/settings.json", "r") as file:
+    data = json.load(file)
 
-unique_dates_list = list(set(dates))
-unique_dates_list.sort()
+new_doc_num = data["next_doc_num"]
+print(f'New Doc Num: {new_doc_num}')
 
-dates.sort()
-
-count_dict = Counter(dates)
-count_list = list(count_dict.values())
-
-count_list_with_dates = list(count_dict.items())
-
-# for item in count_list:
-#     print(item)
-
-# for combo in count_list_with_dates:
-#     print(combo)
-
-# for date in unique_dates_list:
-#     print(date)
-
-fig, ax = plt.subplots()
-
-ax.plot(unique_dates_list, count_list, label='Trends')
-
-ax.set_xlabel('Dates')
-ax.set_ylabel('Count')
-ax.set_title('Trends for dates')
-
-ax.legend()
-
-plt.show()
